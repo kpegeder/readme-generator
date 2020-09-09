31fcb8860345c549b2ab3271f84c622dfd2d4b11
kpegeder <k.pegeder@gmail.com>
2020-09-08 17:47:07 -0700

    add more headings

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,13 +1,27 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let j = [];
+  for (let i = 0; i < 3; i++) {
+    j.push(i);
+  }
+  console.log(j);
   return `
 # ${data.title}
 ## Description
 ${data.description}
 ## Table of Contents
-${data.content}
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+## Installation
+## Usage
+## Credits
 ## License
 ${data.license}
+## Badges
+## Contributing
+## Test
 
 `;
 }
